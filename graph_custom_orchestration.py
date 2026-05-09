5153c60ad5fbdffe627c824aeeeb920476bb9f60
Tahir Mahmood Hashmi <tahirhashmi9090@gmail.com>
2026-05-09 16:56:59 +0500

    Worked with reducers and state management.

--- graph_custom_orchestration.py
+++ graph_custom_orchestration.py
@@ -1,11 +1,14 @@
-from typing import Literal, TypedDict
+from typing import Literal, TypedDict, Annotated
 from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+# from langgraph.graph import MessagesState
 from langgraph.graph.state import CompiledStateGraph # type
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
+from langchain_core.messages import AnyMessage, HumanMessage, SystemMessage, AIMessage
 # from langgraph.prebuilt import create_react_agent
 from langgraph.prebuilt import ToolNode
+from langgraph.prebuilt import tools_condition
 from langchain_tavily import TavilySearch
 
 load_dotenv()  # Ye command khud hi .env file se keys utha legi
@@ -15,12 +18,16 @@ model = ChatGoogleGenerativeAI(
     temperature=0.7  # Ye AI ko thora natural aur conversational banayega
 )
 
+# State definition
+class MessagesState(TypedDict): # Data Structure (Dict), Ye define karta hai ke humare graph ka state kaisa dikhega.
+    messages: Annotated[list[AnyMessage], add_messages]
+
 # Tools setup
 tavily_tool = TavilySearch(max_results=2)
 # Custom tools
-def deposite_money(bankName: str, account: int, amount: float = 1500) -> dict:
+def deposit_money(bankName: str, account: int, amount: float = 1500) -> dict:
     """
-        Deposite money into an account.
+        Deposit money into an account.
         Args:
             bankName (str): Bank name.
             account (int): Account number.
@@ -28,22 +35,17 @@ def deposite_money(bankName: str, account: int, amount: float = 1500) -> dict:
         Returns:
             dict: Confirmation message.
     """
-    print("---Deposite Money Tool Invoked---")
-    return {"message": f"Successfull deposited {amount} into account {account} for {bankName}."}
+    print("---Deposit Money Tool Invoked---")
+    return {"message": f"Successfully deposited {amount} into account {account} for {bankName}."}
 
-tools = [tavily_tool, deposite_money]
+tools = [tavily_tool, deposit_money]
 model_with_tools = model.bind_tools(tools)
 
-tool_node = ToolNode(tools)
-
-# 1. State define karein (ye data nodes ke beech share hoga)
-class GraphState(TypedDict):
-    state: str
-
-def router_function(state: GraphState) -> Literal["tools", "__end__"]:
+# 1. Router function
+def router_function(state: MessagesState) -> Literal["tools", "__end__"]:
     """Ye faisla karta hai ke agla rasta kaunsa hai"""
     print("---Router Function Running---", state)
-    last_message = state["state"][-1]
+    last_message = state["messages"][-1]
     # Agar model ne tool call bheji hai, to 'tools' node par jao
     if last_message.tool_calls:
         return "tools"
@@ -51,47 +53,44 @@ def router_function(state: GraphState) -> Literal["tools", "__end__"]:
     return END
 
 
-# 2. Nodes ke functions banayein
-def node1(current_state: GraphState):
-    print("---Node 1 Running---")
-    input_text = current_state["state"]
+# 2. Nodes
+def node1(current_state: MessagesState) -> MessagesState:
+    print("---Node 1 Running--- ", current_state)
+    response = model.invoke(current_state["messages"])
+    return {"messages": [response]}
 
-    response = model.invoke(f"Please just correct this sentence if needed and just return the corrected one, '{input_text}'.")
-    response = response.content[0]['text']
-    return {"state": response}
+# def node2(current_state: MessagesState) -> MessagesState:
+#     print("---Node 2 Running--- ", current_state)
 
-def node2(current_state: GraphState):
-    input_text = current_state["state"]
-    print("---Node 2 Running--- ", input_text)
-
-    # response = model.invoke(input_text)
-    response = model_with_tools.invoke(input_text)
+#     # response = model.invoke(input_text)
+#     call_response = model_with_tools.invoke(current_state["messages"])
     
-    # content = response["messages"][-1].content
+#     print("Node 2: model_with_tools Response:", call_response)
+#     return {"messages": [call_response]}
 
-    print("Node 2: model_with_tools Response:", response)
-    return {"state": "Good Response from Node 2!"}
+tool_node3 = ToolNode(tools)
 
 # 3. Graph build karein
-workflow: StateGraph = StateGraph(GraphState)
+workflow: StateGraph = StateGraph(MessagesState)
 
 # Nodes add karein
-# workflow.add_node("node1", node1)
-workflow.add_node("node2", node2)
-workflow.add_node("tools", tool_node)
+workflow.add_node("node1", node1)
+# workflow.add_node("node2", node2)
+workflow.add_node("tools", tool_node3)
+
+
 
 # Edges (Raaste) connect karein
-workflow.add_edge(START, "node2") #agent
+workflow.add_edge(START, "node1")
+# workflow.add_conditional_edges("node2", router_function)
+# workflow.add_edge("node1", "node2")
+workflow.add_edge("node1", END)
 
-workflow.add_conditional_edges("node2", router_function)
-
-workflow.add_edge("tools", "node2")
-
-workflow.add_edge("node2", END)
 
 # Graph compile
 graph: CompiledStateGraph = workflow.compile()
 
+initial_input: MessagesState = None
 
 # 4. Run karein
 while True:
@@ -99,10 +98,18 @@ while True:
     if user_input.lower() in ["exit", "quit", "bye"]:
         print("AI: Goodbye!")
         break
-    initial_input = {"state": user_input}
-    result = graph.invoke(initial_input)
 
-    print("\nAI:", result['state'])
+    if initial_input == None:
+        initial_input: MessagesState = {"messages": [
+            SystemMessage(content="Your name is 'Junior'. You are a helpful assistant."), 
+            HumanMessage(content=user_input, name="Tahir")
+            ]}
+    else:
+        initial_input["messages"].append(HumanMessage(content=user_input, name="Tahir"))
+        
+    result = graph.invoke(initial_input)
+    initial_input = result  # Agle input ke liye state update kar do
+    print("\nAI:", result)
 
 
 
